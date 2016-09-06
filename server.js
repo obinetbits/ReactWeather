52e70861d4c8edc765d255949969b0d98226d1e7
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-09-06 08:42:09 +0200

    update middleware for localhost

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url)
+    
+  } else {
+    next();
   }
 });
 
