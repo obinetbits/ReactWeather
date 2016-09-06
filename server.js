38ee9438147012ab4af5eb3380d17aed08d6b3be
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-09-06 08:15:28 +0200

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url)
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000,function () {
-  console.log("Server has started on port 3000");
+app.listen(port, function () {
+  console.log("Server has started on port " + port);
 });
