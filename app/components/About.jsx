38afa67ccd5991a6576dc429de70ae0c6a7cb9e9
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-09-06 18:50:32 +0200

    update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,24 @@ var About = (props) => {
     return (
 
         <div>
-        <h3>About</h3>
-        <p>Welcome to About page!</p>
+        <h1 className="text-center">About</h1>
+            <p>
+                This is a simple yet robust weather application(SPA) built on React. 
+                All you need to use it is enter a city name. 
+                I hope you have fun with it
+            </p>
+            <p>
+                Some of the tools I used are as follows:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+                </li>
+                <li>
+                    <a href="https://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to weather 
+                    data by city name.
+                </li>
+            </ul>
         </div>
     )
 };
