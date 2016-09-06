d6c696a58aaf01e072dde873c6449615d17d7007
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-09-06 08:33:52 +0200

    update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h3>About Component</h3>
+
+        <div>
+        <h3>About</h3>
+        <p>Welcome to About page!</p>
+        </div>
     )
 };
 
