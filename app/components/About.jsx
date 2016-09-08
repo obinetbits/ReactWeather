bee89d64ae9e50696374c5473867e9a5cdc70335
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-09-08 15:49:16 +0200

    add custom css style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = (props) => {
     return (
 
         <div>
-        <h1 className="text-center">About</h1>
+        <h1 className="text-center page-title">About</h1>
             <p>
                 This is a simple yet robust weather application(SPA) built on React. 
                 All you need to use it is enter a city name. 
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -17,7 +17,7 @@ var WeatherForm = React.createClass({
             <div>
                 <form onSubmit={this.onFormSubmit}>
                     <div>
-                        <input type="text" ref="location"/>
+                        <input type="search" ref="location" placeholder="Search weather by city"/>
                     </div>
                     <div>
                         <button className="button expanded hollow">Get Weather</button>
